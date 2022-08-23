3aacbf38b0e12d3459c29648a3bae6ff0191026d
chandrakalaaws <107597669+chandrakalaaws@users.noreply.github.com>
2022-08-23 15:00:25 +0530

    Update CalculatorApplication.java

--- src/main/java/com/coralogix/calculator/CalculatorApplication.java
+++ src/main/java/com/coralogix/calculator/CalculatorApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
+                String name = null;
+		name.toString();
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
